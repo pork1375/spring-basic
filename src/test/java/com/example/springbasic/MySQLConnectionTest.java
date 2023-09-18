282f1592cbcb70185a4f39f9c773e7126e165003
chansung <pork1375@gmail.com>
2023-09-18 22:18:35 +0900

    mysql 연결&테스트

---
+++ src/test/java/com/example/springbasic/MySQLConnectionTest.java
@@ -0,0 +1,28 @@
+package com.example.springbasic;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionTest {
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/study";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234qwer!!";
+
+    @Test
+    public void testConnection() {
+
+        try {
+            Class.forName(DRIVER);
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
